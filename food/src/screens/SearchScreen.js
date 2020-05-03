adbc98e010ab26211f5951baf2a89cdfa49d13a7
shardul <shardul.aeer@somaiya.edu>
2020-05-03 12:12:39 +0530

    lesson: reusable hooks
    Yelp API logic moved to separate file

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchApi = async (searchTerm) => {
-        console.log('hobo');
-        try{
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses);
-        }
-        catch(error) {
-            console.log(error);
-            setErrorMessage('Something went wrong');
-        }
-    }
-    // show some values initially
-    // bad code: component is re-rendered when state is updated.
-    // this will be called in infinite loop.
-
-    // searchApi('pasta');
-    useEffect(() => {
-        searchApi('pasta')
-    }, []);
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
