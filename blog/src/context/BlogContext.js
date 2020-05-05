6d3bc904b5f4e98a384e2e6cddbaaa458e0cb1ef
shardul <shardul.aeer@somaiya.edu>
2020-05-05 13:07:19 +0530

    lesson: Context with Reducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 const BlogContext = React.createContext();
+/**
+ * Reducer function
+ * @param [{title: 'Blog post #1'}] state
+ * @param { type: 'add_post', post: '{title: 'Blog post #2'} } action
+ */
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "add_post": {
+            return [...state, action.post];
+        }
+        default:
+            return state;
+    }
+}
 
 // Provider contains values to be distributed
 export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
+    const [state, dispatch] = useReducer(reducer, []);
 
     function addBlogPost() {
-        setBlogPosts([
-            ...blogPosts,
-            { title: `Blog post #${blogPosts.length + 1}` }
-        ]);
+        dispatch({
+            type: 'add_post',
+            post: { title: `Blog post #${state.length + 1}` }
+        })
     }
 
     //children prop recieves <App />
     return (
-        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+        <BlogContext.Provider value={{ data: state, addBlogPost }}>
             {children}
         </BlogContext.Provider>
     )
